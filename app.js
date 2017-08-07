4dc06cc84d68fee1386d74ab7e03280999b54041
Seana Curran <shaunacurran@shaunas-mbp.attlocal.net>
2017-08-06 23:25:41 -0500

    cart

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Dependecies
 var inquirer = require("inquirer");
 var mysql = require("mysql");
-
+//connect to msql database for bamazon
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -9,13 +9,11 @@ var connection = mysql.createConnection({
     password: "root",
     database: "bamazon_db"
 });
-
 //function to connect to mysql database
 connection.connect(function(error) {
     console.log(error);
     showProducts();
 });
-
 //display products to user
 function showProducts(error, result) {
     connection.query('SELECT * FROM products', function(error, result) {
@@ -48,6 +46,14 @@ function showProducts(error, result) {
                 type: 'input',
                 message: "Please enter the amount you would like."
             }
-        ]);
-    });
-}
+        ]).then(function(cart) {
+                var item= order.id;
+                var quantity = order.stockqty;             
+            } 
+        )
+    }
+});
+
+        
+        
+        
