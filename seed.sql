5b453dce3233ed392b6d14036d3b6b3145cc856d
Shauna Curran <shaunaccurran@gmail.com>
2017-08-07 21:02:35 -0500

    Update seed.sql

--- seed.sql
+++ seed.sql
@@ -1,1 +1,18 @@
+DROP DATABASE IF EXISTS bamazon_db;
+
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR (255) NULL,
+    department_name VARCHAR (255) NULL,
+    price DECIMAL (10,2) NULL,
+    stockqty INTEGER NULL,
+    PRIMARY KEY (id)
+);
+
+
+
 SELECT * FROM bamazon_db.products;
